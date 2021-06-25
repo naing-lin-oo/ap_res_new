920405e1108094c29aa188be3c4a82dc3045fe72
unknown <nainglinoo19497@gmail.com>
2021-06-25 13:34:36 +0630

    auth setup, adminlte setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\DishesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/home', [App\Http\Controllers\OrderController::class, 'index'])->name('home');
+Route::resource('dish', App\Http\Controllers\DishesController::class);
+
+Auth::routes([
+    'register' => false,
+    'reset'    => false,
+    'verify'   => false,
+    'comfirm'   => false,
+]);
